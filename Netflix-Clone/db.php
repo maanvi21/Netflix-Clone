66849e385390fda00d7145022b2d4406fe9cf5ab
Maanvi Shadakshari <maanvishadakshari21@gmail.com>
2025-04-25 17:24:57 +0530

    final

--- Netflix-Clone/db.php
+++ Netflix-Clone/db.php
@@ -11,6 +11,5 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-// Don't echo connection success in production code
-// echo "Connected successfully!";
+
 ?>
